5ea652a7048f14d7f5180a05fcc76a82daf77062
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2016-10-26 04:27:45 +0700

    Initial commit

---
+++ lib/App/TextFragmentUtils.pm
@@ -0,0 +1,65 @@
+package App::TextFragmentUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+use Data::Clone;
+use File::Slurper;
+use Text::Fragment ();
+
+our %SPEC;
+
+my $filename_arg = {
+    schema => 'filename*',
+    req => 1,
+    pos => 0,
+    cmdline_aliases => {f=>{}},
+};
+
+sub _get_tf_args {
+    my %args = @_;
+
+    my %tf_args = %args;
+    my $filename = delete $tf_args{filename};
+    my $text;
+    if ($filename eq '-') {
+        local $/;
+        $text = <STDIN>;
+    } else {
+        $text = read_text($filename);
+    }
+    $tf_args{text} = $text;
+    \%tf_args;
+}
+
+$SPEC{list_fragments} = do {
+    my $meta = clone $Text::Fragment::SPEC{list_fragments};
+    delete $meta->{args}{text};
+    $meta->{args}{filename} = $filename_arg,
+    $meta;
+};
+sub list_fragments {
+    my %args = @_;
+
+    my $tf_args = _get_tf_args(%args);
+    Text::Fragment::list_fragments(%$tf_args);
+}
+
+1;
+#ABSTRACT: CLI utilities related to Text::Fragment
+
+=head1 DESCRIPTION
+
+This distributions provides the following command-line utilities related to
+text fragment:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Text::Fragment>
